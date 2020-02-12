b2f095161ae9102bc1912515da87980a1cc2a409
Phillip Manwaring <phillip.manwaring@gmail.com>
2020-02-11 21:46:59 -0500

    update dynamo default to a single event

--- src/event-mocks/dynamodb-stream.test.ts
+++ src/event-mocks/dynamodb-stream.test.ts
@@ -15,16 +15,16 @@ describe('DynamoDB Stream event', () => {
       ]
     };
     const event = dynamoDBStreamEvent(override);
+    expect(event.Records.length).toEqual(1);
     expect(event.Records[0].eventID).toEqual('3');
     expect(event.Records[0].dynamodb.Keys).toEqual({ Id: { N: '101' } });
     expect(event.Records[0].dynamodb.NewImage.Id).toEqual({ N: '102' });
-    expect(event.Records[1].eventID).toEqual('2');
   });
 
   it('Returns default object when no overrides are specified', () => {
     const event = dynamoDBStreamEvent();
-    expect(event.Records[0].eventID).toEqual('1');
+    expect(event.Records.length).toEqual(1);
+    expect(event.Records[0].eventID).toEqual('2');
     expect(event.Records[0].dynamodb.NewImage.Id).toEqual({ N: '101' });
-    expect(event.Records[1].eventID).toEqual('2');
   });
 });
--- src/event-mocks/dynamodb-stream.ts
+++ src/event-mocks/dynamodb-stream.ts
@@ -23,26 +23,6 @@ function combineMerge(target, source, options) {
 // source: https://docs.aws.amazon.com/lambda/latest/dg/with-ddb.html
 const defaultRecords: DynamoDBRecord[] = [
   {
-    eventID: '1',
-    eventVersion: '1.0',
-    dynamodb: {
-      Keys: {
-        Id: { N: '101' }
-      },
-      NewImage: {
-        Message: { S: 'New item!' },
-        Id: { N: '101' }
-      },
-      StreamViewType: 'NEW_AND_OLD_IMAGES',
-      SequenceNumber: '111',
-      SizeBytes: 26
-    },
-    awsRegion: 'us-west-2',
-    eventName: 'INSERT',
-    eventSourceARN: 'eventsourcearn',
-    eventSource: 'aws:dynamodb'
-  },
-  {
     eventID: '2',
     eventVersion: '1.0',
     dynamodb: {
